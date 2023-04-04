661112d3313268536da8facc73cce7e65a210e67
Artem Laschenkov <laschenkov59@yandex.ru>
2023-04-04 22:59:45 +0300

    FEAT | WELL FITNESS CLIENT | Добавил компонент Htag

---
+++ components/Htag/Htag.tsx
@@ -0,0 +1,14 @@
+import { HtagProps } from "./Htag.props";
+
+export const Htag = ({ tag, children, className }: HtagProps): JSX.Element => {
+  switch (tag) {
+    case "h1":
+      return <h1 className={className}>{children}</h1>;
+    case "h2":
+      return <h2 className={className}>{children}</h2>;
+    case "h3":
+      return <h3 className={className}>{children}</h3>;
+    default:
+      return <></>;
+  }
+};
