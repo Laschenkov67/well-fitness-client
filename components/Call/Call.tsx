cbc31f47bd6907fd8a0be6312eeb28b79813faa1
Artem Laschenkov <laschenkov59@yandex.ru>
2023-04-04 18:15:32 +0300

    FEAT | WELL FITNESS CLIENT | Добавил компонент Call

---
+++ components/Call/Call.tsx
@@ -0,0 +1,14 @@
+import { City } from "../City/City";
+import { CallProps } from "./Call.props";
+import styles from "./Call.module.scss";
+import clsx from "clsx";
+
+export const Call = ({ className }: CallProps): JSX.Element => {
+  return (
+    <div className={clsx(styles.call, className)}>
+      <span className={styles.number}>+7 (800) 000-00-00</span>
+      <City className={styles.city} city="МСК" />
+      <button className={styles.button}>Заказать звонок</button>
+    </div>
+  );
+};
