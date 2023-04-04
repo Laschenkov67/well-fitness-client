da784665922c41f0607b2ba05241517ae98daa83
Artem Laschenkov <laschenkov59@yandex.ru>
2023-04-04 18:16:33 +0300

    FEAT | WELL FITNESS CLIENT | Добавил компонент Card

---
+++ components/Card/Card.tsx
@@ -0,0 +1,20 @@
+import clsx from "clsx";
+import Image from "next/image";
+import { CardProps } from "./Card.props";
+import styles from "./Card.module.scss";
+import { Htag } from "../Htag/Htag";
+
+export const Card = ({ title, image, className }: CardProps): JSX.Element => {
+  return (
+    <div className={clsx(className, styles.card)}>
+      <div>
+        <Htag tag="h3" className={styles.title}>
+          {title}
+        </Htag>
+      </div>
+      <div className={styles.image}>
+        <Image layout="fill" className={styles.img} src={image} alt="" />
+      </div>
+    </div>
+  );
+};
